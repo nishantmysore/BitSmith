59e29149047296d13e047bd4a0fc952e04fc7c91
Nishant Mysore <nishantmysore0@gmail.com>
2024-12-20 20:49:44 -0800

    Updated get all devices service
    Signed-off-by: Nishant Mysore <nishantmysore0@gmail.com>

--- src/lib/services/device.service.ts
+++ src/lib/services/device.service.ts
@@ -29,60 +29,21 @@ export class DeviceService {
         ownerId: session.user.id,
       },
       include: {
-        registers: {
+        peripherals: {
           include: {
-            fields: true,
+            registers: {
+              include: {
+                fields: {
+                  include: {
+                    enumeratedValues: true
+                  }
+              }
+            }
+            }
           },
         },
       },
     });
   }
 
-  static async getDeviceById(id: string) {
-    return prisma.device.findUnique({
-      where: { id },
-      include: {
-        registers: {
-          include: {
-            fields: true,
-          },
-        },
-      },
-    });
-  }
-
-  static async validateDevice(device: Device): Promise<boolean> {
-    if (!device.id || !device.name || !device.description) {
-      return false;
-    }
-
-    const deviceWithRelations = await prisma.device.findUnique({
-      where: { id: device.id },
-      include: {
-        registers: {
-          include: {
-            fields: true,
-          },
-        },
-      },
-    });
-
-    if (!deviceWithRelations?.registers.length) {
-      return false;
-    }
-
-    for (const register of deviceWithRelations.registers) {
-      if (!register.name || !register.address || !register.fields.length) {
-        return false;
-      }
-
-      for (const field of register.fields) {
-        if (!field.name || !field.bits || !field.access || !field.description) {
-          return false;
-        }
-      }
-    }
-
-    return true;
-  }
 }
